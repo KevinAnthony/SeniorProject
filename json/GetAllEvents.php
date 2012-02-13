cf824af98081fc04cb69dffed35e33cc70aca0cc
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-02-13 18:19:16 -0500

    Revert "Restructured the return value of GetAllEvents.php"
    This reverts commit 726aff6b102b0ded13e37be80912622e7e715a1f.

--- json/GetAllEvents.php
+++ json/GetAllEvents.php
@@ -19,14 +19,11 @@ if (!$result){
     $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
 } else {
     $return_array['number_of_rows'] = 0;
-    $data = "[";
     while( $row = mysql_fetch_array($result,MYSQL_ASSOC) ) {
         $temp_array = Array("id" => $row["ID"], "event_name" => $row["EVENT_NAME"],"day" => $row["DAY"],"start_time" => $row["START_TIME"],"end_time" => $row["END_TIME"]);
+        array_push($return_array,$temp_array);
         $return_array['number_of_rows']++;
-        $data=$data.json_encode($temp_array).",";
     }
-    $data = substr($data,0,-1)."]";
-    $return_array["Data"] = $data;
 }
 mysql_free_result($result);
 mysql_close($connection);
