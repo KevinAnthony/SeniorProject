7b375dec68b5265f9b3fa59f767c7e44deac6cb3
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-02-13 16:11:09 -0500

    Added a value to GetAllEvent, which on success of mysql statements, returns the number of rows called number_of_rows

--- json/GetAllEvents.php
+++ json/GetAllEvents.php
@@ -12,14 +12,17 @@ mysql_select_db('ejw3_proj');
 
 $query = "select ID,EVENT_NAME,START_TIME,DAY,END_TIME from EVENT;";
 $result = mysql_query($query);
+
 if (!$result){
     $return_array["success"]=false;
     $error_message = "SQLERROR: Error with query -- ".mysql_error();
     $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
 } else {
+    $return_array['number_of_rows'] = 0;
     while( $row = mysql_fetch_array($result,MYSQL_ASSOC) ) {
         $temp_array = Array("id" => $row["ID"], "event_name" => $row["EVENT_NAME"],"day" => $row["DAY"],"start_time" => $row["START_TIME"],"end_time" => $row["END_TIME"]);
         array_push($return_array,$temp_array);
+        $return_array['number_of_rows']++;
     }
 }
 mysql_free_result($result);
