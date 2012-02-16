66bf24e6fc83686f375280ba6f5f69df6781dbc1
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-02-16 16:54:42 -0500

    Corrected problem with older code

--- json/GetAllEvents.php
+++ json/GetAllEvents.php
@@ -19,14 +19,13 @@ if (!$result){
     $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
 } else {
     $return_array['number_of_rows'] = 0;
-    $data = "[";
+    $data = array();
     while( $row = mysql_fetch_array($result,MYSQL_ASSOC) ) {
-        $temp_array = Array("id" => $row["ID"], "event_name" => $row["EVENT_NAME"],"day" => $row["DAY"],"start_time" => $row["START_TIME"],"end_time" => $row["END_TIME"]);
+        $temp_array = Array("id" => $row["ID"], "event_name" => $row["EVENT_NAME"],"day" => intval($row["DAY"]),"start_time" => intval($row["START_TIME"]),"end_time" => intval($row["END_TIME"]));
+        array_push($data,$temp_array);
         $return_array['number_of_rows']++;
-        $data=$data.json_encode($temp_array).",";
     }
-    $data = substr($data,0,-1)."]";
-    $return_array["Data"] = $data;
+    $return_array['data'] = $data;
 }
 mysql_free_result($result);
 mysql_close($connection);
