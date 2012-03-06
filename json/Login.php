4b8b5744650002897de8423d3cf330e7623c6f46
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-03-06 18:22:01 -0500

    Fixed a big logic error in Login.php in which it logged in everybody

--- json/Login.php
+++ json/Login.php
@@ -1,8 +1,12 @@
 <?php
 $return_array = Array("success" => true);
-$username = $_POST["username"];
-$password = $_POST["password"];
-
+if (isset($_GET["debug"])){
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+} else {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+}
 if (empty($username) or empty($password)){
     $return_array["success"] = false;
     $return_array["error"] = "Parameters not passed username or password";
@@ -20,24 +24,33 @@ if (empty($username) or empty($password)){
     $password_hash = crypt($password,'$1$kevinant$');
     $escaped_username = mysql_real_escape_string($username);
     $escaped_password_hash = mysql_real_escape_string($password_hash);
-    
-    $query = "select count(*) from USER where USERNAME = '$escaped_username' AND PASSWORD = '$escaped_password_hash';";
+    $query = "select * from USER where USERNAME = '$escaped_username' AND PASSWORD = '$escaped_password_hash';";
     $result = mysql_query($query);
     if (!$result){
+        $count = mysql_num_rows($result);
         $return_array["success"]=false;
         $error_message = "SQLERROR: with username or password -- ".mysql_error();
         $return_array["printable_error_message"] = "Invalid Username or Password";
         $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
-    }else if (mysql_num_rows > 1){
-        $return_array["success"]=false;
-        $error_message = "SQLERROR: Search returned more then one row";
-        $return_array["printable_error_message"] = "Invalid Username or Password";
-        $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
-    } else {
-        session_start();
-        $_SESSION['Username'] = $username;
-        $return_array['SID']=session_id();
-        setcookie('SID',session_id());
+    }else{
+        $count = mysql_num_rows($result);
+        if ($count > 1){
+            $return_array["success"]=false;
+            $error_message = "SQLERROR: Search returned more then one row";
+            $return_array["printable_error_message"] = "Invalid Username or Password";
+            $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
+        }else if ($count < 1){
+            $return_array["success"]=false;
+            $error_message = "SQLERROR: Search returned more then no rows";
+            $return_array["printable_error_message"] = "Invalid Username or Password";
+            $return_array["error"]=(empty($return_array["error"]) ? $error_message : $return_array["error"] .';'. $error_message);
+        } else {
+            session_destroy();
+            session_start();
+            $_SESSION['Username'] = $username;
+            $return_array['SID']=session_id();
+            setcookie('SID',session_id());
+        }
     }
     mysql_free_result($result);
     mysql_close($connection);
