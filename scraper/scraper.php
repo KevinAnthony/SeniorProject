96d8a00719e41ceb30d5d05a338d995167f0d239
Erika Waldinger <ejw3@njit.edu>
2012-04-10 13:23:07 -0400

    Cleaned up a little bit

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -1,15 +1,13 @@
 <?php
 
-$url  =  $_SERVER['argv'][1];
-$semester  =  $_SERVER['argv'][2];
-$startPage  =  $_SERVER['argv'][3];
-$numPagesToGet  =  $_SERVER['argv'][4];
+$url = $_SERVER['argv'][1];
+$semester = $_SERVER['argv'][2];
 
 // --------------------------------------------------- BEGIN FIRST PAGE -------------------------------------------//
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $url); 
-curl_setopt($ch, CURLOPT_HEADER, false); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 
@@ -18,53 +16,56 @@ if (!$result) {
 	echo "<br />cURL error number:" .curl_errno($ch);
 	echo "<br />cURL error:" . curl_error($ch);
 	exit;
-} 
-curl_close($ch); 
+}
+curl_close($ch);
 
 echo $result;//show page 1st
 
 $num = substr_count($result, 'Page$');
 
 if ($num > 0) {
-	preg_match_all("/name=\"__VIEWSTATE\" id=\"__VIEWSTATE\" value=\"(.*?)\"/", $result, $arr_viewstate);	
+	$startPage=2;
+	preg_match_all("/name=\"__VIEWSTATE\" id=\"__VIEWSTATE\" value=\"(.*?)\"/", $result, $arr_viewstate);
 	$viewstate=urlencode($arr_viewstate[1][0]);
 	preg_match_all("/name=\"__EVENTVALIDATION\" id=\"__EVENTVALIDATION\" value=\"(.*?)\"/", $result, $arr_eventvalidation);
 	$validation=urlencode($arr_eventvalidation[1][0]);
-	
+
 	while ($num > 0){
 
-		$ch = curl_init(); 
-		curl_setopt($ch, CURLOPT_URL, $url); 
-		curl_setopt($ch, CURLOPT_HEADER, true); 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		
-		curl_setopt($ch, CURLOPT_POSTFIELDS, '__EVENTTARGET='.urlencode('ctl10$GridView1').'&__EVENTARGUMENT='.urlencode("Page$".$startPage).'&__LASTFOCUS=&__VIEWSTATE='.$viewstate.'&__VIEWSTATEENCRYPTED=&__EVENTVALIDATION='.$validation.'&'.urlencode('ctl10$ddlSemester')."=$semester");
+
+		curl_setopt($ch, CURLOPT_POSTFIELDS, '__EVENTTARGET='.urlencode('ctl10$GridView1').'&__EVENTARGUMENT='.			urlencode("Page$".$startPage).'&__LASTFOCUS=&__VIEWSTATE='.$viewstate.'&__VIEWSTATEENCRYPTED=&__EVENTVALIDATION='.$validation.'&'.urlencode('ctl10$ddlSemester')."=$semester");
 		//curl_setopt($ch, CURLOPT_POSTFIELDS, '__EVENTTARGET='.urlencode('ctl10$GridView1').'&__EVENTARGUMENT='.urlencode('Page$2').'&__VIEWSTATE='.$viewstate);
-		
-		
+
+
 		//curl_setopt($ch, CURLOPT_COOKIE, 'ASP.NET_SessionId='.urlencode('a4au0tfh0f3fsdmccnzylg55').'; path=/; domain=courseschedules.njit.edu; HttpOnly');
 		curl_setopt($ch, CURLOPT_COOKIE, 'ASP.NET_SessionId='.urlencode('a4au0tfh0f3fsdmccnzylg55'));
 		curl_setopt($ch, CURLOPT_REFERER, urlencode($url));
-		
-		
-		
+
+
+
 		$result= curl_exec($ch);
 		if (!$result) {
 			echo "<br />cURL error number:" .curl_errno($ch);
 			echo "<br />cURL error:" . curl_error($ch);
 			exit;
-		} 
+		}
 		curl_close($ch);
-		
+
 		$startPage++;$num--;
-		
+
 		echo $result;//show page after posting data
-		
+
 	}
 }
-	
+
 
 
 
 ?>
+
+
